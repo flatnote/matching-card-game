41c2ed75be1a8ca37fed48d463bbcf2178209c20
flatnote <flatsnote@gmail.com>
2023-05-10 08:57:46 +0700

    add vercal analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import "@/styles/globals.scss";
 
 export const metadata = {
@@ -12,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
